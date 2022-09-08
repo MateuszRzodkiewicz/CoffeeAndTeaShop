35c68dc7cc9a5bb122aa361d93a4a8d1f393af58
Mateusz Rzodkiewicz <mateusz.rzodkiewicz8@gmail.com>
2022-09-08 12:19:04 +0200

    fixed divResponse in  SearchInput.tsx

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,12 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Appcontext } from "../context/AppContext";
 import "../css/SearchInput.css";
 import { Product } from "../models/interface";
 function SearchInput({ innerWidth }: { innerWidth: number }): JSX.Element {
   const [inputValue, setInputValue] = useState<string>("");
+  const [flagDiv, setFlagDiv] = useState(false);
+  const divRef = useRef(null);
 
   const products = useContext<Product[] | []>(Appcontext);
 
@@ -13,11 +15,30 @@ function SearchInput({ innerWidth }: { innerWidth: number }): JSX.Element {
   const changeValue = (e: React.FormEvent<HTMLInputElement>) => {
     setInputValue(e.currentTarget.value);
   };
-
-  const showSearchItem = () => {};
+  const showSearchItem = () => {
+    setFlagDiv(true);
+  };
+  const changeFlag = () => {
+    setFlagDiv(false);
+  };
   const dontShowSearchItem = () => {
     setInputValue("");
   };
+
+  function useChageDivFlag(ref: any) {
+    useEffect(() => {
+      function handleClickOutside(event: { target: any }) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setFlagDiv(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  useChageDivFlag(divRef);
 
   const searchProductList = products.filter((product: Product) =>
     product.nameProduct.toLowerCase().includes(inputValue.toLocaleLowerCase())
@@ -41,18 +62,19 @@ function SearchInput({ innerWidth }: { innerWidth: number }): JSX.Element {
   return (
     <div className="inputContainer">
       <input
-        style={{ width: "100%" }}
+        style={{ width: "100%", borderBottom: "solid black 1px " }}
         type={"text"}
         value={inputValue}
         onChange={changeValue}
         placeholder="Wyszukaj swój produkt"
         onFocus={showSearchItem}
       />
-      <div className="searchResponse">
-        <ul> {inputValue && actualySerchItem}</ul>
-      </div>
+      {inputValue && flagDiv && (
+        <div ref={divRef} onBlur={changeFlag} className="searchResponse">
+          <ul> {actualySerchItem}</ul>
+        </div>
+      )}
     </div>
   );
 }
-
 export default SearchInput;
