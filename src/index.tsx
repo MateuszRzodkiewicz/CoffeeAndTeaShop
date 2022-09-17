f2e1901c9a6e4a6270068b53542f5e1b657aa7a6
Mateusz Rzodkiewicz <mateusz.rzodkiewicz8@gmail.com>
2022-09-17 12:58:48 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
+      {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
